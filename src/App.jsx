6f05636cd0ceb956901f372aef14b6eba27a64e9
Revathi-DSAPDev <post2revathi@gmail.com>
2026-01-11 11:10:19 +0530

    Fix react-router import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { Routes, Route,Link, NavLink } from "react-router/dom";
+import { Routes, Route,Link, NavLink } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
